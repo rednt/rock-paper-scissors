0c988557996d82231b421e97dc84924cebd7e6e4
rednt <souhail200ak@gmail.com>
2023-08-19 22:29:09 +0100

    Added game logic for one round

--- rps.js
+++ rps.js
@@ -1,5 +1,5 @@
-function getComputerChoice(x){
-    let rnd = Math.floor(Math.random() * x);
+function getComputerChoice(){
+    let rnd = Math.floor(Math.random() * 3);
     switch (rnd) {
         case 0:
             choice = "Rock"           
@@ -13,6 +13,41 @@ function getComputerChoice(x){
     }
     console.log(choice);
     console.log(rnd);
+    return choice;
 }
 
-getComputerChoice(3);
+function playRound(Player,Computer){
+    if(Player.toLowerCase() == Computer.toLowerCase()){
+        return "Draw | Player: "+ Player + "\nComputer: "+ Computer;
+    }
+    else if(Player.toLowerCase() == "rock" && Computer.toLowerCase() != "rock"){
+        if(Computer.toLowerCase() == "paper"){
+            return "You Lose! Paper beats Rock"+ "\n" + "Player  : " +Player + "\nComputer: "+ Computer;
+        }
+        else{
+            return "You win! Rock beats Scissors"+ "\n" + "Player  : " +Player + "\nComputer: "+ Computer;
+        }
+    }
+    else if(Player.toLowerCase() == "paper" && Computer.toLowerCase() != "paper"){
+        if(Computer.toLowerCase() == "scissors"){
+            return "You Lose! Scissors beats Paper"+ "\n"+"Player  : " +Player + "\nComputer: "+ Computer;
+        }
+        else{
+            return "You win! Paper beats Rock"+ "\n" + "Player  : " + Player + "\nComputer: "+ Computer;
+        }
+    }
+    else if(Player.toLowerCase() == "scissors" && Computer.toLowerCase() != "scissors"){
+        if(Computer.toLowerCase() == "rock"){
+            return "You Lose! Rock beats Scissors"+ "\n"+ "Player  : " +Player + "\nComputer: "+ Computer;
+        }
+        else{
+            return "You win! Scissors beats Paper"+ "\n"+ "Player  : " +Player + "\nComputer: "+ Computer;
+        }
+    }
+}
+
+
+
+const Player = "rock";
+const Computer = getComputerChoice();
+console.log(playRound(Player, Computer));
